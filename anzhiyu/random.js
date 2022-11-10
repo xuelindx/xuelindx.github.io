85e2b2425a4be6ecbf874ef5e04e028f61bdd8bb
xuelindx <1741529170@qq.com>
2022-11-11 07:41:18 +0800

    Site updated: 2022-11-11 07:41:17

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/1f1d5fa4.html","posts/db112152.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/undefined.html","posts/db112152.html","posts/1f1d5fa4.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
