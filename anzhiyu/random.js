20b8590a1b3a862d7910452b9c751ad3d8976660
xuelindx <1741529170@qq.com>
2023-03-30 19:19:37 +0800

    Site updated: 2023-03-30 19:19:37

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/db112152.html","posts/1f1d5fa4.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/a3d9.html","posts/1f1d5fa4.html","posts/db112152.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
