68f8c6769d6883495f7985e6c7f1e968f0fc38df
xuelindx <1741529170@qq.com>
2023-05-18 19:53:27 +0800

    Site updated: 2023-05-18 19:53:27

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/a3d9.html","posts/db112152.html","posts/37ef.html","posts/1f1d5fa4.html","posts/187c.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/a3d9.html","posts/db112152.html","posts/1f1d5fa4.html","posts/37ef.html","posts/187c.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
